1d17e0de3d4603890e7e4629da3e78b44a22c178
Tong Zhang <warriorlance@gmail.com>
2014-05-05 21:38:38 +0800

    update readdpa_tdp c source

--- Readdpa_tdp/getSlices.h
+++ Readdpa_tdp/getSlices.h
@@ -4,13 +4,25 @@
 #ifndef _GETSLICES_H
 #define _GETSLICES_H
 
+template <class T>
+std::string num2str(T x)
+{
+	std::stringstream sstr;
+	sstr << x;
+	return sstr.str();
+}
+
+int parseOpts(int argc, char* argv[], std::string &file1, std::string &file2)
+
+
 class getSliceInfo
 {
 	private:
 		unsigned int start_slice, end_slice; // the number of the starting and ending slice to be extracted;
 	public:
-
+}
 		
+
 
 
 
