3774fa864329dffc2c8849f0212642d50136c339
Tong Zhang <warriorlance@gmail.com>
2014-05-20 10:08:55 +0800

    update two main cpp files

--- Getdp/getdp_s.cpp
+++ Getdp/getdp_s.cpp
@@ -17,7 +17,7 @@
  * along with this program. If not see <http://www.gnu.org/licenses/>.*
 \*********************************************************************/
 
-/******************************************************************************\
+/*********************************************************************\
 This program is written for parsing output file (TDP)
 
 ***Get the given slices z-value
@@ -30,17 +30,10 @@ Usage: getdp file1 file2 slice_order
 Author: Tong ZHANG
 e-mail: tzhang@sinap.ac.cn
 Created Time: 10:05, Dec. 9th, 2011
-\*******************************************************************************/
+\*********************************************************************/
 
+#include "getdp_fun.h"
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <cstdlib>
-#include <cstring>
-
-const int MAX_CHARS_PER_LINE = 512;
-const int MAX_TOKENS_PER_LINE = 20;
-const char* DELIMITER = " \t";
 
 using namespace std;
 
@@ -51,40 +44,47 @@ int main(int argc, char **argv)
 	{
 		if (argc >= 2) cout << "Not enough parameters!\n\n";
 		cout << "Usage: " << argv[0] << " file1 file2 s-pos\n";
-		cout << "\t" << "file1: "     << "\t\t" << "TDP output filename, open to read\n";
-		cout << "\t" << "file2: "     << "\t\t" << "data filename, open to write\n";
-		cout << "\t" << "s-pos:"      << "\t"   << "s-pos in [m]\n\n";
+		cout << "\t" << "file1: "     << "\t" << "TDP output filename, open to read\n";
+		cout << "\t" << "file2: "     << "\t" << "data filename, open to write\n";
+		cout << "\t" << "s-pos:"      << "\t" << "s-pos in unit of [m]\n\n";
 		exit(1);
 	}
 
-	ifstream file1(argv[1]); 		// tdp filename to read
-	ofstream file2(argv[2]); 		// filename for writting data
+	string file1name = argv[1]; // tdp filename to read
+	string file2name = argv[2]; // filename for writting data
+
+	ifstream file1;
+	ofstream file2;
+
+	double s_pos = atof(argv[3]); 	  // s-pos in [m]
+	string keystr = "seperation";     // define the keyword string, to find its value
+	double slice_sepe = findKeywordValue(file1name, keystr);
+	int s_order = s_pos/slice_sepe+1; // transform s_pos into s-record order
+
+	file1.open(file1name.c_str(), ifstream::in);
+	file2.open(file2name.c_str(), ofstream::out);
 
 	if (!file1 || !file2) 			// if any one of files cannot open, return error, exit
 	{
 		cout << "Open file error!\n\n";
 		exit(1);
 	}
-	
-	double s_pos = atof(argv[3]); 	  // s-pos in [m]
-	double slice_sepe = findKeywordValue(file1, "seperation");
-	int s_order = s_pos/slice_sepe+1; // transform s_pos into s-record order
 
 // locate the slice [s-order]
 	int count = 0; // read line by line, increase count value when encounter line starts with "*", untile count = s-order
-	while(!file1.eof()) // do not stop reading untile the end of file1 
+	while (!file1.eof()) // do not stop reading untile the end of file1 
 	{
-		if(count==s_order)break;
+		if (count==s_order)break;
 		char buf[MAX_CHARS_PER_LINE];
 		file1.getline(buf, MAX_CHARS_PER_LINE);
 		char* token[MAX_TOKENS_PER_LINE] = {0};
-		token[0] = strtok(buf, DELIMITER);
+		token[0] = strtok(buf, DELIMITER.c_str());
 		if(token[0] && token[0][0]=='*')count++;
 	}
 
 	//pass next 5 useless lines
 	string line;
-	for(int i =1;i<=5;i++)
+	for (int i =1; i<=5; i++)
 	{
 		getline(file1, line);
 		//file2 << line << "\n";
@@ -92,7 +92,7 @@ int main(int argc, char **argv)
 
 	//extract the [s-order]th slice, 'result v.s. z'
 	getline(file1, line);
-	while(!line.empty())
+	while (!line.empty())
 	{
 		file2 << line << "\n";
 		getline(file1, line);
--- Getdp/getdp_z.cpp
+++ Getdp/getdp_z.cpp
@@ -33,14 +33,7 @@ Created Time: 14:58, Dec. 9th, 2011
 \*******************************************************************************/
 
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <cstdlib>
-#include <cstring>
-
-const int MAX_CHARS_PER_LINE = 512;
-const int MAX_TOKENS_PER_LINE = 30;
-const char* DELIMITER = " \t";
+#include "getdp_fun.h"
 
 using namespace std;
 
@@ -50,35 +43,46 @@ int main(int argc, char **argv)
 	if(argc != 4) // if the number of parameters is not enough, then give errors, exit
 	{
 		if(argc >=2)cout << "Not enough parameters!\n\n";
-		cout << "Usage: " << argv[0] << " file1 file2 z-order\n";
-		cout << "\t" << "file1: " << "\t\t" << "TDP output filename, open to read\n";
-		cout << "\t" << "file2: " << "\t\t" << "data filename, open to write\n";
-		cout << "\t" << "z-order:"<< "\t"   << "z-axis order number\n\n";
+		cout << "Usage: " << argv[0] << " file1 file2 z-pos\n";
+		cout << "\t" << "file1: " << "\t" << "TDP output filename, open to read\n";
+		cout << "\t" << "file2: " << "\t" << "data filename, open to write\n";
+		cout << "\t" << "z-pos:"  << "\t" << "z-pos in unit of [m]\n\n";
 		exit(1);
 	}
 
-	ifstream file1(argv[1]); 		// tdp filename to read
-	ofstream file2(argv[2]); 		// filename for writting data
-	int z_order = atoi(argv[3]); 	// z-record order
+	string file1name = argv[1]; // tdp filename to read
+	string file2name = argv[2];	// filename for writting data
+
+	ifstream file1;
+	ofstream file2;
+
+	double z_pos = atof(argv[3]); // z-pos in [m]
+	string keystr1 = "delz";
+	string keystr2 = "xlamd";
+	double delz  = findMainKeyValue(file1name, keystr1);
+	double xlamd = findMainKeyValue(file1name, keystr2);
+	int z_order = z_pos/(delz*xlamd)+1; 	// transform z-pos into z-record order
+	
+	file1.open(file1name.c_str(), ifstream::in);
+	file2.open(file2name.c_str(), ofstream::out);
 
 	if (!file1 || !file2) 			// if any one of files cannot open, return error, exit
 	{
 		cout << "Open file error!\n\n";
 		exit(1);
 	}
-	
 
 	int count = 0; // read line by line, increase count value when encounter line starts with "*", means find one slice record
-	while(!file1.eof()) // do not stop reading untile the end of file1 
+	while (!file1.eof()) // do not stop reading untile the end of file1 
 	{
 		// locate the beginning of every slice [s-order]
-		while(1)
+		while (1)
 		{
 			char buf[MAX_CHARS_PER_LINE];
 			file1.getline(buf, MAX_CHARS_PER_LINE);
 			char* token[MAX_TOKENS_PER_LINE] = {0};
-			token[0] = strtok(buf, DELIMITER);
-			if(token[0] && token[0][0]=='*')
+			token[0] = strtok(buf, DELIMITER.c_str());
+			if (token[0] && token[0][0]=='*')
 			{
 				count++;
 				break;
@@ -87,7 +91,7 @@ int main(int argc, char **argv)
 
 		//pass next 5 useless lines
 		string line;
-		for(int i =1;i<=5;i++)
+		for (int i =1; i<=5; i++)
 		{
 			getline(file1, line);
 			//file2 << line << "\n";
@@ -96,10 +100,10 @@ int main(int argc, char **argv)
 		//extract the [slice_order]th z-record, given that line_count = z_order
 		int line_count = 0;
 		getline(file1, line);
-		while(!line.empty())
+		while (!line.empty())
 		{
 			line_count++;
-			if(line_count==z_order)file2 << line << "\n";
+			if (line_count==z_order) file2 << line << "\n";
 			getline(file1, line);
 		}
 	}
