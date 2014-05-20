dc9872b1aad1f69be14f08025fec3a2be92311db
Tong Zhang <warriorlance@gmail.com>
2014-05-20 10:07:31 +0800

    add function dToE and fine keywords from outputfile

--- Getdp/getdp_fun.cpp
+++ Getdp/getdp_fun.cpp
@@ -1,25 +1,61 @@
 #include "getdp_fun.h"
+#include <iostream>
 
-double findKeywordValue(ifstream &infile, string &keystr)
+double findKeywordValue(std::string &filename, std::string &keystr)
 {
+	std::ifstream infile;
+	infile.open(filename.c_str(), std::ifstream::in);
+	if(!infile)
+	{
+		std::cout << filename << " can not open!" << std::endl;
+		exit(1);
+	}
     char buf[MAX_CHARS_PER_LINE];
     char* tokenTmp1;
     char* tokenTmp2;
     while (!infile.eof())
     {
         infile.getline(buf, MAX_CHARS_PER_LINE);
-        char* token = strtok(buf, DELIMITER);
+        char* token = strtok(buf, DELIMITER.c_str());
         if (token)
         {
 			tokenTmp1 = token;
-			if((tokenTmp2 = strtok(NULL, DELIMITER)) && !strcmp(tokenTmp2, keystr.c_str()))
-			{
-				cout << tokenTmp1 << endl;
-				break;
-			}
+			if((tokenTmp2 = strtok(NULL, DELIMITER.c_str())) && !strcmp(tokenTmp2, keystr.c_str())) break;
 		}
 	}
 	infile.close();
 	return atof(tokenTmp1);
+}
+
+double findMainKeyValue(std::string &filename, std::string &keystr)
+{
+	std::ifstream infile;
+	infile.open(filename.c_str(), std::ifstream::in);
+	if(!infile)
+	{
+		std::cout << filename << " can not open!" << std::endl;
+		exit(1);
+	}
+	char buf[MAX_CHARS_PER_LINE];
+	char* tokenTmp1;
+	char* tokenTmp2;
+	while (!infile.eof())
+	{
+		infile.getline(buf, MAX_CHARS_PER_LINE);
+		char* token = strtok(buf, DELIMITER.c_str());
+		if (token)
+		{
+			tokenTmp1 = token;
+			if(!strcmp(tokenTmp1, keystr.c_str()) && (tokenTmp2 = strtok(NULL, DELIMITER.c_str()))) break;
+		}
+	}
+	infile.close();
+	return atof(dToE(std::string(tokenTmp2)).c_str());
+}
+
+std::string dToE(std::string cppstr)
+{
+	cppstr.replace(cppstr.find("D"),1,"E");
+	return cppstr;
 }
 
--- Getdp/getdp_fun.h
+++ Getdp/getdp_fun.h
@@ -1,21 +1,26 @@
 #ifndef _GETDP_FUN_H
 #define _GETDP_FUN_H
 
-#include <ifstream>
+#include <fstream>
 #include <string>
 #include <cstdlib>
+#include <cstring>
 
 // const list
 const int MAX_CHARS_PER_LINE  = 512;
 const int MAX_TOKENS_PER_LINE = 20; 
-const char* DELIMITER = " \t";
+const std::string DELIMITER = " \t=";
 
-double findKeywordValue(std::ifstream &infile, std::string &keystr);
+double findKeywordValue(std::string &filename, std::string &keystr);
 // keystr list:
 // entries
 // history
 // wavelength
 // seperation
 // meshsize
+
+double findMainKeyValue(std::string &filename, std::string &keystr);
+
+std::string dToE(std::string cppstr);
 
 #endif //_GETDP_FUN_H
